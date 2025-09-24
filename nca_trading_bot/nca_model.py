a3a24c0cd4fa4bb97a67960fe7f74489d9d38d93
mkfdj <mautlej6@gmail.com>
2025-09-24 17:51:40 +0200

    ss

--- nca_trading_bot/nca_model.py
+++ nca_trading_bot/nca_model.py
@@ -16,6 +16,20 @@ from jax.scipy.signal import convolve2d
 from functools import partial
 import time
 from collections import deque
+import tensorflow as tf
+
+try:
+    tpu = tf.distribute.cluster_resolver.TPUClusterResolver(tpu="local") # TPU detection
+    tf.config.experimental_connect_to_cluster(tpu)
+    tf.tpu.experimental.initialize_tpu_system(tpu)
+    strategy = tf.distribute.TPUStrategy(tpu)
+    print("TPU detected")
+except ValueError:
+    print("No TPU detected")
+    strategy = tf.distribute.get_strategy()
+    print("Number of devices:", strategy.num_replicas_in_sync, "🚀")
+
+from config import get_config
 
 from config import get_config
 
